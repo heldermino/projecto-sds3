9babb6dcf1163772175c84ddafe19a843bf31387
Helder Minó <jomino32@gmail.com>
2021-09-14 18:48:00 +0100

    Bootstrap

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+(index.tsx) import 'bootstrap/dist/css/bootstrap.css';
+import 'assets/css/styles.css';
 import App from './App';
 
 ReactDOM.render(
